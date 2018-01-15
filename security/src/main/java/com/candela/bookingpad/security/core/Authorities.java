a262c452a42ccc437466972471a79dc3c4f765ea
Dilan Anushka <dilan.anushka@pearson.com>
2018-01-15 23:39:02 +0530

    code refactoring

--- security/src/main/java/com/candela/bookingpad/security/core/Authorities.java
+++
@@ -1,9 +0,0 @@
-package com.candela.bookingpad.security.core;
-
-public enum Authorities {
-
-    ROLE_ANONYMOUS,
-    ROLE_USER,
-    ROLE_ADMIN
-
-}
--- security/src/main/java/com/candela/bookingpad/security/repository/AuthorityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.candela.bookingpad.security.repository;
-
-
-import com.candela.bookingpad.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
